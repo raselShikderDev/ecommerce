3a5b3e07200c63a90b512049ff87c40571e84f02
Rasel-Shikder <rasel.sikder777.rk@gmail.com>
2025-03-03 20:31:03 +0600

    added authentication by clerk - 0.1.2

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,22 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import { assets } from "@/assets/assets";
 import Link from "next/link";
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
+import { useClerk, UserButton, useUser } from "@clerk/nextjs";
 
 const Navbar = () => {
-  const { isSeller, router } = useAppContext();
+  const { isSeller, router, user } = useAppContext();
+  const { openSignIn } = useClerk();
+  const { isSignedIn } = useUser();
 
+   const handleSellerDashboard = () =>{
+      if (isSignedIn) {
+        router.push("/seller")
+      }
+      router.push(openSignIn())
+   }
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
       <Link
@@ -16,7 +25,8 @@ const Navbar = () => {
         href={"/"}
         alt="logo"
       >
-        <span className="font-medium text-orange-600">S</span>hop<span className="font-medium text-black">W</span>here
+        <span className="font-medium text-orange-600">S</span>hop
+        <span className="font-medium text-black">W</span>here
       </Link>
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
         <Link href="/" className="hover:text-gray-900 transition">
@@ -34,7 +44,8 @@ const Navbar = () => {
 
         {isSeller && (
           <button
-            onClick={() => router.push("/seller")}
+            // onClick={() => router.push("/seller")}
+            onClick={handleSellerDashboard}
             className="text-xs border px-4 py-1.5 rounded-full"
           >
             Seller Dashboard
@@ -44,10 +55,17 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4 ">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
-          <Image src={assets.user_icon} alt="user icon" />
-          Account
-        </button>
+        {isSignedIn ? (
+          <UserButton />
+        ) : (
+          <button
+            onClick={openSignIn}
+            className="flex items-center gap-2 hover:text-gray-900 transition"
+          >
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
       </ul>
 
       <div className="flex items-center md:hidden gap-3">
@@ -59,10 +77,14 @@ const Navbar = () => {
             Seller Dashboard
           </button>
         )}
-        <button className="flex items-center gap-2 hover:text-gray-900 transition">
-          <Image src={assets.user_icon} alt="user icon" />
-          Account
-        </button>
+        {isSignedIn ? (
+          <UserButton />
+        ) : (
+          <button className="flex items-center gap-2 hover:text-gray-900 transition">
+            <Image src={assets.user_icon} alt="user icon" />
+            Account
+          </button>
+        )}
       </div>
     </nav>
   );
--- components/seller/Navbar.jsx
+++ components/seller/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { assets } from '../../assets/assets'
 import Image from 'next/image'
 import { useAppContext } from '@/context/AppContext'
+import Link from 'next/link'
 
 const Navbar = () => {
 
@@ -9,7 +10,14 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center px-4 md:px-8 py-3 justify-between border-b'>
-      <Image onClick={()=>router.push('/')} className='w-28 lg:w-32 cursor-pointer' src={assets.logo} alt="" />
+      <Link
+        className="cursor-pointer text-gray-800 w-28 text-3xl md:text-4xl"
+        onClick={() => router.push("/")}
+        href={"/"}
+        alt="logo"
+      >
+        <span className="font-medium text-orange-600">S</span>hop<span className="font-medium text-black">W</span>here
+      </Link>
       <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
